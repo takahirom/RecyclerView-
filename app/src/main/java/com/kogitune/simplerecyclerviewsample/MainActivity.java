04f5770309537025665630bf2ca476ef0f8cc169
takahirom <takam.dev@gmail.com>
2015-02-27 00:04:14 +0900

    add source

---
+++ app/src/main/java/com/kogitune/simplerecyclerviewsample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.kogitune.simplerecyclerviewsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class MainActivity extends Activity {
+
+    private static final int DATASET_COUNT = 60;
+    private String[] mDataset;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDataset();
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        CustomAdapter adapter = new CustomAdapter(mDataset);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
+    }
+}
